6f44af3e65b976f65af27f28354ef8629d04adc2
ardie85@yahoo.com <ardie85@yahoo.com>
2020-03-26 16:04:47 +0700

    commit lagi

--- src/halaman/Blacklist/Blacklist_view.js
+++ src/halaman/Blacklist/Blacklist_view.js
@@ -1,51 +1,51 @@
-import React, { Component } from 'react';
-import { Card, CardHeader, CardBody } from 'reactstrap'
-import {} from './Form'
-import { SubJudul2 } from '../reuse/Typografi';
-import DataTable, {} from 'react-data-table-component';
-import { PageJudul } from '../reuse/Page';
+// import React, { Component } from 'react';
+// import { Card, CardHeader, CardBody } from 'reactstrap'
+// import {} from './Form'
+// import { SubJudul2 } from '../reuse/Typografi';
+// import DataTable, {} from 'react-data-table-component';
+// import { PageJudul } from '../reuse/Page';
 
-const paginationOptions = { rowsPerPageText: 'Tampilkan data per', rangeSeparatorText: 'dari' };
+// const paginationOptions = { rowsPerPageText: 'Tampilkan data per', rangeSeparatorText: 'dari' };
 
-class Index extends Component {
-state = { }
-render() {
+// class Index extends Component {
+// state = { }
+// render() {
     
-    const data = [{ id: 1, title: 'Conan the Barbarian', year: '1982' }];
-    const columns = [
-    {
-    name: 'Title',
-    selector: 'title',
-    sortable: true,
-    },
-    {
-    name: 'Year',
-    selector: 'year',
-    sortable: true,
-    right: true,
-    },
-    ];
-return (
-    <div>
-<PageJudul title="List Pengajuan Blacklist" deskripsiPage="Berisikan list data pengajuan blacklist dari Distributor"/>
-<Card>
-           <CardBody >
+//     const data = [{ id: 1, title: 'Conan the Barbarian', year: '1982' }];
+//     const columns = [
+//     {
+//     name: 'Title',
+//     selector: 'title',
+//     sortable: true,
+//     },
+//     {
+//     name: 'Year',
+//     selector: 'year',
+//     sortable: true,
+//     right: true,
+//     },
+//     ];
+// return (
+//     <div>
+// <PageJudul title="List Pengajuan Blacklist" deskripsiPage="Berisikan list data pengajuan blacklist dari Distributor"/>
+// <Card>
+//            <CardBody >
            
-            <DataTable
-          columns= {columns}
-          data = {data}
-          pagination
-          paginationComponentOptions={paginationOptions}
-        //   selectableRows
-          noHeader
-          persistTableHead
-        ></DataTable>
-            </CardBody>
-          </Card>
-    </div>
-);
-}
-}
+//             <DataTable
+//           columns= {columns}
+//           data = {data}
+//           pagination
+//           paginationComponentOptions={paginationOptions}
+//         //   selectableRows
+//           noHeader
+//           persistTableHead
+//         ></DataTable>
+//             </CardBody>
+//           </Card>
+//     </div>
+// );
+// }
+// }
 
 
-export default Index;
+// export default Index;
